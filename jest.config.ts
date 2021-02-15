8b532221a9bbb2ebeb141c1f69b339cc5065ddb3
Nestor <junior110120@gmail.com>
2021-02-15 12:29:05 -0500

    ci: migrate jest setup js to typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   setupFiles: ['<rootDir>/tests/setup.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/node_modules/**']
 }
+export default config
