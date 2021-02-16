c28c0139797992ff95fe42a04a39bd145611c4fb
Nestor <junior110120@gmail.com>
2021-02-15 22:52:57 -0500

    refactor: building dynamic moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,25 @@
 import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest/utils'
+const {
+  compilerOptions: { paths: tsconfigPaths }
+} = require('./tsconfig.json')
+
+const paths = {
+  ...pathsToModuleNameMapper(tsconfigPaths)
+}
+Object.assign(
+  paths,
+  ...Object.keys(paths).map((key) => ({
+    [key]: `<rootDir>/src/${paths[key]}`
+  }))
+)
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   setupFiles: ['<rootDir>/tests/setup.ts'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/node_modules/**']
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/node_modules/**'],
+  moduleNameMapper: paths
 }
 export default config
