a6c7057704359abda387bcf121161c6480521869
Nestor Cortina <junior110120@gmail.com>
2021-05-02 00:09:36 -0500

    ci: create executable

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -4,10 +4,9 @@ import path from 'path'
 const projectRoot = path.join(__dirname, '..', 'build', 'package.json')
 const packageJSON = fse.readJSONSync(projectRoot)
 packageJSON.scripts = {
-  start: 'node ./server.js'
+  start: 'node ./server.js',
+  pkg: 'pkg ./package.json --output ./pkg/server'
 }
 Reflect.deleteProperty(packageJSON, 'devDependencies')
-Reflect.deleteProperty(packageJSON, 'bin')
-Reflect.deleteProperty(packageJSON, 'pkg')
 
 fse.writeFileSync(projectRoot, JSON.stringify(packageJSON, null, '  '))
